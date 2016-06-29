5f23f33cb97b003b07e268ed1271fa0078623765
JocieShen <JocelynShenBB@gmail.com>
2016-06-29 09:58:02 -0700

    even newer changes

--- mSrc/getBinFreq.m
+++ mSrc/getBinFreq.m
@@ -1,6 +1,20 @@
 function [ bins, frequency ] = getBinFreq( arr)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%getBinFreq: function that creates the bins and the frequency to plot the
+%scaling exponents
+%
+%INPUT:
+% arr: array containing the scaling exponents
+%
+%OUTPUT:
+%  bins
+%  frequency: frequency that each variable occurs
+%
+%EXAMPLES
+% [bins, frequency] = getBinFreq(arr);
+%
+%HISTORY	
+%	Jocie Shen, 6/23/16, first written
+%============================================================
 bins = logspace(log10(min(arr)), log10(max(arr)));
 frequency = [];
 
